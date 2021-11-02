66fbdc129f8e63a02a3465e815f38af1b48318f3
Alejandro Martín Cancelos <alcancelos@gmail.com>
2021-11-02 13:52:56 -0300

    Texto

--- js/script.js
+++ js/script.js
@@ -3,42 +3,43 @@ const regex = /^[0-9]*$/;
 
 function ObtenerTransporte() {
 
-    let ValorIngresado = document.getElementById("Distancia").value
-
+    let Metros = document.getElementById("Distancia").value
+    let Result = document.getElementById("Respuesta");
+    let img = document.getElementById("Imagen");
     //Valido que sea un entero
-    if (Number.isInteger(parseInt(ValorIngresado)) && regex.test(ValorIngresado)) {
-        if (ValorIngresado >= 0 && ValorIngresado <= 1000) {
-            document.getElementById("Respuesta").innerHTML = "La distancia de " + ValorIngresado + " metros debe recorrerse a pie"
-            document.getElementById("Imagen").innerHTML = '<img src="./images/pie.png" />'
-            document.getElementById("Respuesta").classList.add("Ok");
-            document.getElementById("Respuesta").classList.remove("Error");
-        } else if (ValorIngresado > 1000 && ValorIngresado <= 10000) {
-            document.getElementById("Respuesta").innerHTML = "La distancia de " + ValorIngresado + " metros debe recorrerse en bicicleta"
-            document.getElementById("Imagen").innerHTML = '<img src="./images/bicicleta.png" />'
-            document.getElementById("Respuesta").classList.add("Ok");
-            document.getElementById("Respuesta").classList.remove("Error");
-        } else if (ValorIngresado > 10000 && ValorIngresado <= 30000) {
-            document.getElementById("Respuesta").innerHTML = "La distancia de " + ValorIngresado + " metros debe recorrerse en colectivo"
-            document.getElementById("Imagen").innerHTML = '<img src="./images/colectivo.png" />'
-            document.getElementById("Respuesta").classList.add("Ok");
-            document.getElementById("Respuesta").classList.remove("Error");
-        } else if (ValorIngresado > 30000 && ValorIngresado <= 100000) {
-            document.getElementById("Respuesta").innerHTML = "La distancia de " + ValorIngresado + " metros debe recorrerse en auto"
-            document.getElementById("Imagen").innerHTML = '<img src="./images/auto.png" />'
-            document.getElementById("Respuesta").classList.add("Ok");
-            document.getElementById("Respuesta").classList.remove("Error");
-        } else if (ValorIngresado > 100000) {
-            document.getElementById("Respuesta").innerHTML = "La distancia de " + ValorIngresado + " metros debe recorrerse en avión"
-            document.getElementById("Imagen").innerHTML = '<img src="./images/avion.png" />'
-            document.getElementById("Respuesta").classList.add("Ok");
-            document.getElementById("Respuesta").classList.remove("Error");
+    if (Number.isInteger(parseInt(Metros)) && regex.test(Metros)) {
+        if (Metros >= 0 && Metros <= 1000) {
+            Result.innerHTML = "La distancia de " + Metros + " metros debe recorrerse a pie"
+            img.innerHTML = '<img src="./images/pie.png" />'
+            Result.classList.add("Ok");
+            Result.classList.remove("Error");
+        } else if (Metros > 1000 && Metros <= 10000) {
+            Result.innerHTML = "La distancia de " + Metros + " metros debe recorrerse en bicicleta"
+            img.innerHTML = '<img src="./images/bicicleta.png" />'
+            Result.classList.add("Ok");
+            Result.classList.remove("Error");
+        } else if (Metros > 10000 && Metros <= 30000) {
+            Result.innerHTML = "La distancia de " + Metros + " metros debe recorrerse en colectivo"
+            img.innerHTML = '<img src="./images/colectivo.png" />'
+            Result.classList.add("Ok");
+            Result.classList.remove("Error");
+        } else if (Metros > 30000 && Metros <= 100000) {
+            Result.innerHTML = "La distancia de " + Metros + " metros debe recorrerse en auto"
+            img.innerHTML = '<img src="./images/auto.png" />'
+            Result.classList.add("Ok");
+            Result.classList.remove("Error");
+        } else if (Metros > 100000) {
+            Result.innerHTML = "La distancia de " + Metros + " metros debe recorrerse en avión"
+            img.innerHTML = '<img src="./images/avion.png" />'
+            Result.classList.add("Ok");
+            Result.classList.remove("Error");
         }
     }
     else {
-        document.getElementById("Respuesta").innerHTML = "El formato ingresado no es correcto"
-        document.getElementById("Imagen").innerHTML = '<img src="./images/interrogacion.png" />'
-        document.getElementById("Respuesta").classList.add("Error");
-        document.getElementById("Respuesta").classList.remove("Ok");
+        Result.innerHTML = "El formato ingresado no es correcto"
+        img.innerHTML = '<img src="./images/interrogacion.png" />'
+        Result.classList.add("Error");
+        Result.classList.remove("Ok");
     }
 }
 
@@ -47,6 +48,7 @@ function ObtenerTransporte() {
 //Primer Metodo usando Math
 function ObtenerMayor() {
 
+    let Result = document.getElementById("Respuesta2");
     //Cargo el array con los numeros de los campos de texto
     var Numeros = [document.getElementById("num1").value,
     document.getElementById("num2").value,
@@ -57,19 +59,20 @@ function ObtenerMayor() {
     let resultado = Math.max.apply(null, Numeros);
     //si el resultado es distinto de null lo muestro, sino hay algun numero mal ingresado
     if (!Number.isNaN(resultado)) {
-        document.getElementById("Respuesta2").innerHTML = "El mayor número ingresado es: " + resultado;
-        document.getElementById("Respuesta2").classList.add("Ok");
-        document.getElementById("Respuesta2").classList.remove("Error");
+        Result.innerHTML = "El mayor número ingresado es: " + resultado;
+        Result.classList.add("Ok");
+        Result.classList.remove("Error");
     } else {
-        document.getElementById("Respuesta2").innerHTML = "Ingresaste mal algún número"
-        document.getElementById("Respuesta2").classList.add("Error");
-        document.getElementById("Respuesta2").classList.remove("Ok");
+        Result.innerHTML = "Ingresaste mal algún número"
+        Result.classList.add("Error");
+        Result.classList.remove("Ok");
     }
 }
 
 //Segundo metodo usando un for y recorriendo el array
 function ObtenerMayor2() {
 
+    let Result = document.getElementById("Respuesta2");
     //Cargo el array con los numeros de los campos de texto
     var Numeros = [document.getElementById("num1").value,
     document.getElementById("num2").value,
@@ -94,14 +97,14 @@ function ObtenerMayor2() {
     }
 
     if (!error) { //Si no hay error muestro el mayor
-        document.getElementById("Respuesta2").innerHTML = "El mayor número ingresado es: " + max;
-        document.getElementById("Respuesta2").classList.add("Ok");
-        document.getElementById("Respuesta2").classList.remove("Error");
+        Result.innerHTML = "El mayor número ingresado es: " + max;
+        Result.classList.add("Ok");
+        Result.classList.remove("Error");
     } else {
         //Si hay error aviso que se ingresó mal algún número
-        document.getElementById("Respuesta2").innerHTML = "Ingresaste mal algún número"
-        document.getElementById("Respuesta2").classList.add("Error");
-        document.getElementById("Respuesta2").classList.remove("Ok");
+        Result.innerHTML = "Ingresaste mal algún número"
+        Result.classList.add("Error");
+        Result.classList.remove("Ok");
     }
 
 }
